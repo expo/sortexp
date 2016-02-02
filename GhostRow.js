04449215bafd8b1e112e9a0e22d0529d69705b77
Brent Vatne <brentvatne@gmail.com>
2016-02-01 16:26:19 -0800

    Style in state, sad times

--- GhostRow.js
+++ GhostRow.js
@@ -12,39 +12,38 @@ var {
 var GhostRow = React.createClass({
 
   getInitialState: function() {
-    // TODO: yikes, reaching into another component's state
-    let activeRowId = this.props.list.state.activeRowId;
-    let layout = this.props.list.state.activeLayout;
-    let rowLayout = this.props.list.layoutMap[this.props.rowData.rowId];
-
+    // TODO: fade it out when we release
     return {
-      style: {
-        position: 'absolute',
-        elevation: 3,
-        left: 0,
-        right: 0,
-        height: layout.frameHeight,
-        overflow: 'hidden',
-        backgroundColor: 'transparent',
-        borderWidth: 0.5,
-        borderColor: '#eee',
-        marginTop: layout.pageY - 20, // Account for top bar spacing
-      }
-    }
+    };
   },
 
   render: function() {
     let item = this.props.renderRow(
       this.props.rowData.data,
       this.props.rowData.rowId,
-      {
-        active: true,
-      }
+      { active: true },
     );
 
+    // TODO: yikes, reaching into another component's state
+    let rowId = this.props.list.state.activeRowId;
+    let layout = this.props.list.state.activeLayout;
+    let rowLayout = this.props.list.layoutMap[rowId];
+
+    let style = {
+      position: 'absolute',
+      elevation: 3,
+      left: 0,
+      right: 0,
+      height: layout.frameHeight,
+      overflow: 'hidden',
+      backgroundColor: 'transparent',
+      borderWidth: 0.5,
+      borderColor: '#eee',
+      marginTop: layout.pageY - 20, // Account for top bar spacing (???)
+    };
+
     return (
-      <Animated.View
-        style={[this.state.style, {top: this.props.list.state.panY}]}>
+      <Animated.View style={[style, {top: this.props.list.state.panY}]}>
         {item}
       </Animated.View>
     );
